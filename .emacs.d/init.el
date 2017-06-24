e298b2a7e7b3dde9b191d4944b8c1d832d110278
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-06-24 13:54:56 +0200

    Command to re-load all open files in Emacs.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -352,3 +352,14 @@
 ;  (quail-define-rules ((append . t))
 ;                      ("^\\alpha" ?ᵅ)))
 ;                      ("\\ir" ?ℝ)
+
+
+(defun revert-all-buffers ()
+    "Refreshes all open buffers from their respective files."
+    (interactive)
+    (dolist (buf (buffer-list))
+      (with-current-buffer buf
+        (when (and (buffer-file-name) (file-exists-p (buffer-file-name)) (not (buffer-modified-p)))
+          (revert-buffer t t t) )))
+    (message "Refreshed open files.") )
+ (global-set-key (kbd "C-x R") 'revert-all-buffers)
